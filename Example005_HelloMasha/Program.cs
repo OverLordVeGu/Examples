bb2d0dcb060112b6141c7c6e9ac0863b85adee6b
VeGu <VeGu@bk.ru>
2023-07-19 22:22:19 +0700

    Добавили условие в приветствие

---
+++ Example005_HelloMasha/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+if (username.ToLower() == "кира") {
+    Console.WriteLine("Приветствую моя родная!!!");}
+else
+{   Console.Write("Ну здравствуй ");
+    Console.WriteLine (username);
+    }
